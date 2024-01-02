4d4770bbaf8788930c5949792ba70de415fd126b
Minhh17 <nguyenhaiminhr@gmail.com>
2024-01-02 16:54:38 +0700

    add add student gui

--- src/routes/student.routes.js
+++ src/routes/student.routes.js
@@ -3,13 +3,7 @@ const router = express.Router();
 const studentController = require('../app/controllers/StudentController'); // la doi tuong nen viet thuong, class moi viet hoa chu cai dau
 
 router.get('/', studentController.index);  // boc tach ro rang controller va vs model
-router.get('/getData', studentController.getDataFromDB);  // boc tach ro rang controller va vs model
-
-router.get('/seeData', (req, res) => {
-    let data = studentController.getDataFromDB;
-    console.log(data);
-    res.render('student', {data: data});
-    
-});
+router.get('/getData', studentController.getDataFromDB); 
+router.get('/create', studentController.create);  
 
 module.exports = router;
